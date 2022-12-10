6a702483eac631cf415d2f9d0440a6a4d630245d
Rosie <rosie.bradshaw3@gmail.com>
2022-12-10 18:23:09 +0100

    Saxon tests pasados

--- src/saxon/Saxon.ts
+++ src/saxon/Saxon.ts
@@ -1,3 +1,21 @@
-export class Saxon {
 
-}
+import {Soldier} from '../soldier/Soldier'
+
+export class Saxon extends Soldier {
+    constructor(health:number, strength:number){
+    super(health, strength);
+    }
+
+    attack():number{
+        return this.strength;
+    }
+
+    receiveDamage(damage: number):any{
+        this.health = this.health-damage;
+        
+        if (this.health>0) return `A Saxon has received ${damage} points of damage`;
+        else return `A Saxon has died in combat`
+    } 
+
+
+};     
