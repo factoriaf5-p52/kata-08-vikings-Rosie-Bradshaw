5c22e24345387c13193a4e49659f887ff84febfe
Rosie <rosie.bradshaw3@gmail.com>
2022-12-12 09:04:31 +0100

    quiero pasar el trabajo al ordenador de mesa simplemente

--- src/war/War.ts
+++ src/war/War.ts
@@ -1,2 +1,19 @@
+ import { Saxon } from "../saxon/Saxon";
+ import { Viking } from "../viking/Viking";
+
 export class War{
-}
+    
+    vikingArmy:Viking[] = []
+    saxonArmy:Saxon[] = []
+     
+    addViking(viking:Viking){
+        this.vikingArmy.push(viking); 
+        return undefined;
+    }
+
+    addSaxon(saxon:Saxon){
+        this.saxonArmy.push(saxon); 
+        return undefined;
+    }
+    
+}    
